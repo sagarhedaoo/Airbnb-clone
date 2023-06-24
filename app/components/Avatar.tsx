17d70143969bed7fe9637c38d4275bda4d23ed6c
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-06-24 10:37:06 -0400

    Bug fixes

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,10 +1,14 @@
 "use client";
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image
-      src="/images/placeholder.jpg"
+      src={src || "/images/placeholder.jpg"}
       alt="Avatar"
       className="rounded-full"
       height="30"
